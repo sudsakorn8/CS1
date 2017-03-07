1dddd89e10d64dfe4bbdfa86085a4888fa32fe0d
sudsakorn8 <373929746@qq.com>
2017-03-07 10:24:02 +0800

    加入了绘制理论曲线代码

--- CS/Pd_SNR.m
+++ CS/Pd_SNR.m
@@ -8,10 +8,11 @@ SNR = -20:2:0;
 SNR_num = length(SNR);
 CS_ratio = 0.5;
 CS_num = length(CS_ratio);
-trial_num = 30;
+trial_num = 1;
 %trial_num = 1; % trial number for calculating each Pd
 P_f = zeros(SNR_num,CS_num); % detection probability with different SNR and Compression ratio
 P_d = zeros(SNR_num,CS_num);
+P_d_th =zeros(SNR_num,CS_num);
 % Pd_denoised = zeros(SNR_num,CS_num);
 Pd_occupied = zeros(SNR_num,CS_num);
 error = zeros(trial_num,2);
@@ -28,7 +29,7 @@ for n = 1:CS_num
     for i = 1:SNR_num
         for j = 1:trial_num
             fprintf('CS_ratio=%f,SNR=%f,trial_num=%d  \n',CS_ratio(n),SNR(i),j);
-            [pd_trial(j,1),pd_trial(j,2),idx_Channel(:,i),if_vacant(:,i),E(:,i),E_ori(:,i),lamda_d(:,i),a_r(:,i)] = CSS_Denoised_CS( SNR(i),CS_ratio(n),Sparsity,Pf);
+            [pd_trial(j,1),pd_trial(j,2),idx_Channel(:,i),if_vacant(:,i),E(:,i),E_ori(:,i),lamda_d(:,i),P_d_th(i,n),a_r(:,i)] = CSS_Denoised_CS( SNR(i),CS_ratio(n),Sparsity,Pf);
 %             [error(j,1),error(j,2),a_r,a_r_d] = Denoised_CS( SNR(i),CS_ratio(n),Sparsity,Pf);
 %             [error(j,1),error(j,2)] = CSS_Denoised_CS( SNR(i),CS_ratio(n),Sparsity,Pf);
         end
@@ -42,6 +43,7 @@ for n = 1:CS_num
     figure;
     plot(SNR,P_f(:,1),'o-b'); hold on
     plot(SNR,P_d(:,1),'square-b'); hold on
+    plot(SNR,P_d_th(:,1),'*-b'); hold on
     xlabel('SNR(dB)'); ylabel('P');
     % legend('Traditional CS based SS 20%','Traditional CS based SS 25%','Denoised CS based SS 20%','Denoised CS based SS 25%','Location','SouthEast');
     legend('P_f','P_d','Location','SouthEast');
--- CS/Plot_3D.m
+++ CS/Plot_3D.m
@@ -3,16 +3,16 @@ CS_ratio = [50,40,30,25,20,15,10,5];
 SNR = -20:2:0;
 CS_ratio_mesh = 10:10:50;
 [x,y] = meshgrid(SNR,CS_ratio_mesh);
-% z(1,:)=struct_data_10.P_d(1:2:21);
-% z(2,:)=struct_data_20.P_d(1:2:21);
-% z(3,:)=struct_data_30.P_d;
-% z(4,:)=struct_data_40.P_d;
-% z(5,:)=struct_data_50.P_d;
-z(1,:)=struct_data_10.P_f(1:2:21);
-z(2,:)=struct_data_20.P_f(1:2:21);
-z(3,:)=struct_data_30.P_f;
-z(4,:)=struct_data_40.P_f;
-z(5,:)=struct_data_50.P_f;
+z(1,:)=struct_data_10.P_d(1:2:21);
+z(2,:)=struct_data_20.P_d(1:2:21);
+z(3,:)=struct_data_30.P_d;
+z(4,:)=struct_data_40.P_d;
+z(5,:)=struct_data_50.P_d;
+% z(1,:)=struct_data_10.P_f(1:2:21);
+% z(2,:)=struct_data_20.P_f(1:2:21);
+% z(3,:)=struct_data_30.P_f;
+% z(4,:)=struct_data_40.P_f;
+% z(5,:)=struct_data_50.P_f;
 figure;
 for CS_ratio_num = 1:length(CS_ratio)
     var_str = ['struct_data_',num2str(CS_ratio(CS_ratio_num))];
@@ -22,7 +22,7 @@ for CS_ratio_num = 1:length(CS_ratio)
 %     scatter3(temp.SNR,CS_ratio(CS_ratio_num)*ones(1,SNR_num),temp.P_d);hold on;
     surf(x,y,z);
 end
-title('Pf-SNR-CSratio based on Traditional CSS' );
+title('Pd-SNR-CSratio based on Traditional CSS' );
 x1=xlabel('SNR(dB)');
 x2=ylabel('CS ratio');
 zlabel('P_d');
